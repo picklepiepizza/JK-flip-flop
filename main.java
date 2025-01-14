beb0ba37c1c403da2f318809940bf41f9532b34b
Jason Gedlu <57841746+JGedlu@users.noreply.github.com>
2025-01-14 18:45:18 -0500

    Update main.java
    Updated JK Flip-Flop functionality to handle logical cases

--- main.java
+++ main.java
@@ -1,15 +1,11 @@
-public class Main
-{
-	public static void main(String[] args) {
-		
-		JK newJK = new JK(1, 0, 0, 0);
-		
-		newJK.clock();
-		
-		System.out.println(newJK.getq());
-
-		// SET = 1
-		// RESET = 0
-		
-	}
+public class Main {
+    public static void main(String[] args) {
+        
+        JK newJK = new JK(true, false, true, true);
+        
+        newJK.clock();
+        
+        System.out.println(newJK.getq());
+        System.out.println(newJK.getqnot());
+    }
 }
