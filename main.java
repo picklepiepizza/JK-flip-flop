5b917c19fc76bb3091bacde5d35e26521e5cf616
picklepiepizza <37784720+picklepiepizza@users.noreply.github.com>
2025-01-16 14:13:15 -0500

    Update main.java

--- main.java
+++ main.java
@@ -16,7 +16,7 @@ public class Main
 		    
 		    for (int currentInput=0;currentInput<=3;currentInput++) {
 		        
-		        System.out.print("Enter "+inputs[currentInput]+": ");
+		        System.out.print("Enter "+inputs[currentInput]+" (0-1): ");
 		        
 		        int chosenInput = Integer.parseInt(scan.nextLine());
 		        settings[currentInput] = chosenInput!=0;
@@ -31,7 +31,9 @@ public class Main
 		    boolean q = outputs[0];
 		    boolean qnot = outputs[1];
 		    
-		    System.out.println(JK.clock(MyJK)[0]);
+		    System.out.println("Outputs");
+		    System.out.println("Q: "+q);
+		    System.out.println("Qn: "+qnot);
 		    
 		}
 		
