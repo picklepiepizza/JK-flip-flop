a7ff18e1e292aadfa84569a32085e35468eaa116
picklepiepizza <37784720+picklepiepizza@users.noreply.github.com>
2025-01-16 14:06:37 -0500

    Update JK.java

--- JK.java
+++ JK.java
@@ -16,38 +16,53 @@ public class JK {
         this.clear = clear;
     }
     
-    public void setValues(boolean set, boolean reset, boolean preset, boolean clear) {
+    public void setInputs(boolean set, boolean reset, boolean preset, boolean clear) {
         this.set = set;
         this.reset = reset;
         this.preset = preset;
         this.clear = clear;
     }
     
-    public void clock() {
+    public boolean[] getInputs() {
+        boolean[] inputs = {this.set, this.reset, this.preset, this.clear};
+        return inputs;
+    }
+    
+    public static boolean[] clock(JK jk) {
         
-        if (this.preset == false && this.clear == false) {
-            this.q = true;
-            this.qnot = true;
-        } else if (this.preset == false && this.clear == true) {
-            this.q = true;
-            this.qnot = false;
-        } else if (this.preset == true && this.clear == false) {
-            this.q = false;
-            this.qnot = true;
-        } else if (this.preset == true && this.clear == true) {
+        // Q, Qn
+        boolean[] outputs = {jk.getq(), jk.getqnot()};
+        boolean[] inputs = jk.getInputs();
+        boolean set = inputs[0];
+        boolean reset = inputs[1];
+        boolean preset = inputs[2];
+        boolean clear = inputs[3];
+        
+        if (preset == false && clear == false) {
+            outputs[0] = true;
+            outputs[1] = true;
+        } else if (preset == false && clear == true) {
+            outputs[0] = true;
+            outputs[1] = false;
+        } else if (preset == true && clear == false) {
+            outputs[0] = false;
+            outputs[1] = true;
+        } else if (preset == true && clear == true) {
             
-            if (this.set == false && this.reset == true) {
-                this.q = false;
-                this.qnot = true;
-            } else if (this.set == true && this.reset == false) {
-                this.q = true;
-                this.qnot = false;
-            } else if (this.set == true && this.reset == true) {
-                this.q = !this.q;
-                this.qnot = !this.qnot;
+            if (set == false && reset == true) {
+                outputs[0] = false;
+                outputs[1] = true;
+            } else if (set == true && reset == false) {
+                outputs[0] = true;
+                outputs[1] = false;
+            } else if (set == true && reset == true) {
+                outputs[0] = !outputs[0];
+                outputs[1] = !outputs[1];
             }
             
         }
+        
+        return outputs;
         
     }
     
