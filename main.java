af28379d43714b13109d150962c700f9bb573cc2
picklepiepizza <37784720+picklepiepizza@users.noreply.github.com>
2025-01-22 13:41:36 -0500

    Update main.java

--- main.java
+++ main.java
@@ -8,32 +8,61 @@ public class Main
 		
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Choose an option\n (1) Test JK flip flop\n (2) Test counter");
+		int option = Integer.parseInt(scan.nextLine());
+		
 		JK MyJK = new JK();
 		
-		while (true) {
+		if (option == 1) {
 		    
-		    boolean[] settings = {false, false, false, false};
+    		while (true) {
+    		    
+    		    boolean[] settings = {false, false, false, false};
+    		    
+    		    for (int currentInput=0;currentInput<=3;currentInput++) {
+    		        
+    		        System.out.print("Enter "+inputs[currentInput]+" (0-1): ");
+    		        
+    		        int chosenInput = Integer.parseInt(scan.nextLine());
+    		        settings[currentInput] = chosenInput!=0;
+    		        
+    		    }
+    		    
+    		    MyJK.setInputs(settings[0], settings[1], settings[2], settings[3]);
+    		    
+    		    // clock
+    		    
+    		    boolean[] outputs = JK.clock(MyJK);
+    		    System.out.println("Outputs"+"\nQ: "+outputs[0]+"\nQn: "+outputs[1]);
+    		    
+    		    MyJK.setq(outputs[0]);
+    		    MyJK.setqnot(outputs[1]);
+    		    
+    		}
+    		
+		} else if (option == 2) {
 		    
-		    for (int currentInput=0;currentInput<=3;currentInput++) {
+		    System.out.println("Input counter length (in JK flip flops)");
+		    int counterLength = Integer.parseInt(scan.nextLine());
+		    
+		    Counter myCounter = new Counter(counterLength);
+		    
+		    while (true) {
 		        
-		        System.out.print("Enter "+inputs[currentInput]+" (0-1): ");
-		        
-		        int chosenInput = Integer.parseInt(scan.nextLine());
-		        settings[currentInput] = chosenInput!=0;
-		        
+		        System.out.println(" (1) Pulse counter\n (2) Read counter");
+		        int counterOption = Integer.parseInt(scan.nextLine());
+    		    
+    		    if (counterOption == 1) {
+    		        myCounter.pulse();
+    		        System.out.println("Pulsed counter");
+    		    } else {
+    		        System.out.println("Current value: "+myCounter.read());
+    		    }
+        		
 		    }
-		    
-		    MyJK.setInputs(settings[0], settings[1], settings[2], settings[3]);
-		    
-		    // clock
-		    
-		    boolean[] outputs = JK.clock(MyJK);
-		    System.out.println("Outputs"+"\nQ: "+outputs[0]+"\nQn: "+outputs[1]);
-		    
-		    MyJK.setq(outputs[0]);
-		    MyJK.setqnot(outputs[1]);
 		    
 		}
 		
 	}
+	
 }
